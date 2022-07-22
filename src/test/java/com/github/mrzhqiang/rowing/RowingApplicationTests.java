131d5fe700d865fef4833395b76f92caecd944b5
张强 <287431404@qq.com>
2022-07-22 10:56:18 +0800

    first commit.

---
+++ src/test/java/com/github/mrzhqiang/rowing/RowingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mrzhqiang.rowing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
