5cf90e3360cfb2a1e140d25f8a0c6c7087954a07
张强 <287431404@qq.com>
2022-07-26 09:37:08 +0800

    [src] 新增 账户及登录注册控制器

---
+++ src/main/java/com/github/mrzhqiang/rowing/core/account/LoginFailureListener.java
@@ -0,0 +1,58 @@
+package com.github.mrzhqiang.rowing.core.account;
+
+import com.github.mrzhqiang.rowing.config.SecurityProperties;
+import com.github.mrzhqiang.rowing.util.Authentications;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nonnull;
+import java.time.Duration;
+import java.time.Instant;
+
+@Component
+public class LoginFailureListener implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+
+    private final SecurityProperties securityProperties;
+    private final AccountRepository accountRepository;
+
+    public LoginFailureListener(SecurityProperties securityProperties,
+                                AccountRepository accountRepository) {
+        this.securityProperties = securityProperties;
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(@Nonnull AuthenticationFailureBadCredentialsEvent event) {
+        Authentication authentication = event.getAuthentication();
+        handleLoginFailed(authentication);
+    }
+
+    private void handleLoginFailed(Authentication authentication) {
+        Authentications.findUsername(authentication)
+                .flatMap(accountRepository::findByUsername)
+                .filter(Account::isAccountNonLocked)
+                .map(this::computeFailedCount)
+                .ifPresent(accountRepository::save);
+    }
+
+    private Account computeFailedCount(Account account) {
+        Instant firstFailed = account.getFirstFailed();
+        Instant now = Instant.now();
+        Duration firstFailedDuration = securityProperties.getFirstFailedDuration();
+        if (firstFailed == null || firstFailed.plus(firstFailedDuration).isBefore(now)) {
+            account.setFirstFailed(now);
+            account.setFailedCount(1);
+            return account;
+        }
+
+        int hasFailedCount = account.getFailedCount() + 1;
+        account.setFailedCount(hasFailedCount);
+        if (hasFailedCount >= securityProperties.getMaxLoginFailed()) {
+            Duration duration = securityProperties.getLockedDuration();
+            account.setLocked(now.plus(duration));
+        }
+        return account;
+    }
+}
---
+++ src/main/java/com/github/mrzhqiang/rowing/core/account/PasswordModifyForm.java
@@ -0,0 +1,30 @@
+package com.github.mrzhqiang.rowing.core.account;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * 密码修改表单。
+ * <p>
+ * 此表单用于修改密码，需要提供旧密码验证是否具备修改权限。
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PasswordModifyForm extends PasswordConfirmForm {
+
+    /**
+     * 旧密码。
+     * <p>
+     * 用于验证是否具备修改权限，与 password 字段的校验规则及要求完全相同。
+     */
+    @NotBlank
+    @Size(min = 6, max = 15)
+    @ToString.Exclude
+    @JsonIgnore
+    private String oldPassword;
+}
