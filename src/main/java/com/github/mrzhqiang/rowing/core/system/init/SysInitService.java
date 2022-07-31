834ecd7f1b7c3c535bab5469bcbb200041b8dba7
mrzhqiang <287431404@qq.com>
2022-07-31 21:07:36 +0800

    [java] 添加 系统初始化服务

---
+++ src/main/java/com/github/mrzhqiang/rowing/core/system/init/SysInitService.java
@@ -0,0 +1,22 @@
+package com.github.mrzhqiang.rowing.core.system.init;
+
+import com.github.mrzhqiang.rowing.core.system.AutoInitializer;
+
+import java.util.List;
+
+public interface SysInitService {
+
+    /**
+     * 检测自动初始化器列表。
+     * <p>
+     * 1. 通过 {@link AutoInitializer#getName()} 判断是否存在对应名称的系统初始化数据，如果存在则忽略
+     * <p>
+     * 2. 按照以上规则，得到未记录的自动初始化器列表，如果列表为空，则跳过检测，如果列表不为空，则执行下一步
+     * <p>
+     * 3. 逐个转换自动初始化器为系统初始化实体，并保存到数据库，完成检测操作
+     *
+     * @param initializerList 自动初始化器列表。
+     * @return 已保存到数据库的系统初始化数据列表。
+     */
+    List<SysInit> check(List<AutoInitializer> initializerList);
+}
---
+++ src/main/java/com/github/mrzhqiang/rowing/core/system/init/SysInitServiceJpaImpl.java
@@ -0,0 +1,39 @@
+package com.github.mrzhqiang.rowing.core.system.init;
+
+import com.github.mrzhqiang.rowing.core.system.AutoInitializer;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SysInitServiceJpaImpl implements SysInitService {
+
+    private final SysInitMapper mapper;
+    private final SysInitRepository repository;
+
+    public SysInitServiceJpaImpl(SysInitMapper mapper,
+                                 SysInitRepository repository) {
+        this.mapper = mapper;
+        this.repository = repository;
+    }
+
+    @Override
+    public List<SysInit> check(List<AutoInitializer> initializers) {
+        if (CollectionUtils.isEmpty(initializers)) {
+            return Collections.emptyList();
+        }
+
+        List<SysInit> sysInits = initializers.stream()
+                .filter(it -> !repository.existsByName(it.getName()))
+                .map(mapper::toEntity)
+                .collect(Collectors.toList());
+        if (CollectionUtils.isEmpty(sysInits)) {
+            return Collections.emptyList();
+        }
+
+        return repository.saveAll(sysInits);
+    }
+}
