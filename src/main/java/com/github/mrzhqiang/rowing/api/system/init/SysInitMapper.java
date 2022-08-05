ec9f90660272582a7232ef1b262b182807b39f26
mrzhqiang <287431404@qq.com>
2022-08-05 21:07:23 +0800

    [java] 修复 编译报错的问题

--- src/main/java/com/github/mrzhqiang/rowing/api/system/init/SysInitMapper.java
+++ src/main/java/com/github/mrzhqiang/rowing/api/system/init/SysInitMapper.java
@@ -1,17 +1,18 @@
 package com.github.mrzhqiang.rowing.api.system.init;
 
+import com.github.mrzhqiang.rowing.api.domain.TaskStatus;
 import com.github.mrzhqiang.rowing.api.system.AutoInitializer;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", imports = TaskStatus.class)
 public interface SysInitMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "version", ignore = true)
     @Mapping(target = "type", expression = "java( SysInit.Type.REQUIRED )")
     @Mapping(target = "name", source = "initializer.name")
-    @Mapping(target = "status", expression = "java( SysInit.Status.DEFAULT )")
+    @Mapping(target = "status", expression = "java( TaskStatus.DEFAULT )")
     @Mapping(target = "ordered", source = "initializer.order")
     SysInit toEntity(AutoInitializer initializer);
 }
--- src/main/java/com/github/mrzhqiang/rowing/api/system/setting/SysSettingGroupForm.java
+++ src/main/java/com/github/mrzhqiang/rowing/api/system/setting/SysSettingGroupForm.java
@@ -9,4 +9,6 @@ public class SysSettingGroupForm {
 
     @NotBlank
     private String name;
+    @NotBlank
+    private String code;
 }
