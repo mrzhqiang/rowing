47327be978f4fbef91d9e57b7ea69386150cf1c7
张强 <287431404@qq.com>
2022-08-12 17:58:23 +0800

    [java] 重构 根据文档重构字典组和字典项实体

--- src/main/java/com/github/mrzhqiang/rowing/system/data/DataDictGroup.java
+++ src/main/java/com/github/mrzhqiang/rowing/system/data/DataDictGroup.java
@@ -17,7 +17,9 @@ import java.util.List;
 /**
  * 字典组。
  * <p>
- * 分组是为了给字典分类，因为可能存在多个字典项相同，但部分字典项不同的情况，此时如果混杂在一起，不利于筛选。
+ * 分组是为了给字典分类。
+ * <p>
+ * 主要支持不同的字典组（分类）拥有相同的字典项名称，但字典项值不同的情况。
  */
 @Getter
 @Setter
@@ -31,24 +33,6 @@ public class DataDictGroup extends AuditableEntity {
     private String code;
     @Enumerated(EnumType.STRING)
     private Type type;
-    /**
-     * 字典组来源。
-     * <p>
-     * 1. 内置的默认类型，来源是 classpath 上的内置数据字典 Excel 文件绝对路径。
-     * <p>
-     * 2. 系统管理的 DB 类型，来源是操作用户的用户名。
-     * <p>
-     * 3. 第三方 API 类型，来源是对应的 URL 地址。
-     * <p>
-     * 来源仅作为数据参考，不具备使用价值。
-     */
-    private String source;
-    /**
-     * 序列号。
-     * <p>
-     * 以 yyyyMMddHHmmss 格式作为序列号。
-     */
-    private String serialNo;
 
     /**
      * 组内的字典项列表。
@@ -68,16 +52,10 @@ public class DataDictGroup extends AuditableEntity {
          */
         DEFAULT,
         /**
-         * DB 字典组。
+         * 自定义字典组。
          * <p>
          * 通过系统手动添加或修改，则被设置为此类型。
          */
-        DB,
-        /**
-         * API 字典组。
-         * <p>
-         * 从第三方 API 接口获取的数据字典，则被设置为此类型。
-         */
-        API,
+        CUSTOMIZE,
     }
 }
--- src/main/java/com/github/mrzhqiang/rowing/system/data/DataDictItem.java
+++ src/main/java/com/github/mrzhqiang/rowing/system/data/DataDictItem.java
@@ -22,41 +22,18 @@ import javax.persistence.ManyToOne;
 @Entity
 public class DataDictItem extends AuditableEntity {
 
-    /**
-     * 图标。
-     * <p>
-     * 这个字段是可选的，用来在 label 前面显示图标。
-     */
     private String icon;
-    /**
-     * 标签。
-     * <p>
-     * 字典项的名称，用于展示。
-     */
     @Column(nullable = false)
     private String label;
-    /**
-     * 值。
-     * <p>
-     * 字典项的值，用于查询。
-     */
     @Column(nullable = false)
     private String value;
-    /**
-     * 序列号。
-     * <p>
-     * 以 yyyyMMddHHmmss 格式作为序列号。
-     * <p>
-     * 通常序列号来自组序列号，除非是通过系统新增的字典项。
-     */
-    private String serialNo;
 
     /**
      * 字典组。
      * <p>
      * 当前字典项所在组。
      */
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @ToString.Exclude
     private DataDictGroup group;
 }
