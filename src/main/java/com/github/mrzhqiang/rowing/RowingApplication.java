c5eda0f23afd55477c98101d9f0aa8f704a272f7
mrzhqiang <287431404@qq.com>
2023-03-26 01:41:35 +0800

    [java] 优化：启动类现在监听启动成功和失败事件，而不是使用古老的事件监听接口

--- src/main/java/com/github/mrzhqiang/rowing/RowingApplication.java
+++ src/main/java/com/github/mrzhqiang/rowing/RowingApplication.java
@@ -3,23 +3,29 @@ package com.github.mrzhqiang.rowing;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationFailedEvent;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
-
-import javax.annotation.Nonnull;
+import org.springframework.context.event.EventListener;
 
 @Slf4j
 @SpringBootApplication
-public class RowingApplication implements ApplicationListener<ApplicationReadyEvent> {
+public class RowingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RowingApplication.class, args);
     }
 
-    @Override
-    public void onApplicationEvent(@Nonnull ApplicationReadyEvent event) {
+    @EventListener
+    public void onEvent(ApplicationReadyEvent event) {
         log.info("=============================================");
-        log.info("           系统启动完毕，可以正常使用！         ");
+        log.info("     系统启动完毕，用时 {}，现在可以正常使用！    ", event.getTimeTaken());
         log.info("=============================================");
+    }
+
+    @EventListener
+    public void onEvent(ApplicationFailedEvent event) {
+        log.error("=============================================");
+        log.error("           系统启动失败，请检查错误：           ", event.getException());
+        log.error("=============================================");
     }
 }
