38f553ecc4bf16933756ca6758fb1865857f5b12
mrzhqiang <287431404@qq.com>
2023-04-05 22:40:18 +0800

    [java] 删除导致无法启动的多余的配置类

--- src/main/java/com/github/mrzhqiang/rowing/config/RestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.github.mrzhqiang.rowing.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-
-@EnableSpringDataWebSupport
-@Configuration
-public class RestConfiguration {
-}
