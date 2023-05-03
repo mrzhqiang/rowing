1c3d9ec51e03ded8eb8c3de14fd251e2e57fec67
mrzhqiang <287431404@qq.com>
2023-05-03 18:54:52 +0800

    优化配置项，调试模式下正确打印sql参数

--- src/main/java/com/github/mrzhqiang/rowing/RowingApplication.java
+++ src/main/java/com/github/mrzhqiang/rowing/RowingApplication.java
@@ -5,8 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationFailedEvent;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.convert.DurationStyle;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 
+import java.time.temporal.ChronoUnit;
+
+/**
+ * 行舟在线考试系统。
+ */
 @Slf4j
 @SpringBootApplication
 public class RowingApplication {
@@ -15,17 +22,12 @@ public class RowingApplication {
         SpringApplication.run(RowingApplication.class, args);
     }
 
+    @Order
     @EventListener
     public void onEvent(ApplicationReadyEvent event) {
-        log.info("=============================================");
-        log.info("系统启动完毕，用时 {}s，现在可以正常使用！    ", event.getTimeTaken().getSeconds());
-        log.info("=============================================");
-    }
-
-    @EventListener
-    public void onEvent(ApplicationFailedEvent event) {
-        log.error("=============================================");
-        log.error("系统启动失败，请检查错误：", event.getException());
-        log.error("=============================================");
+        log.info("===========================================");
+        String time = DurationStyle.SIMPLE.print(event.getTimeTaken(), ChronoUnit.SECONDS);
+        log.info("系统启动完毕，用时 {}，现在可以正常使用！", time);
+        log.info("===========================================");
     }
 }
